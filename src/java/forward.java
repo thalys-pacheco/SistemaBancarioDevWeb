03543b09bb87c282c6c4085c5b6723524bc50f4c
Diego Camargo <diego.camargo@injunior.com.br>
2023-11-09 16:19:42 -0300

    Ajeitando forward.java

--- src/java/forward.java
+++ src/java/forward.java
@@ -1,20 +1,18 @@
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/banco"})
-public class banco extends HttpServlet {    
+@WebServlet(urlPatterns = {"/forward"})
+public class forward extends HttpServlet {    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        String nome = request.getParameter("nome");
-        try (PrintWriter out = response.getWriter()) {
-            //RequestDispatcher rd = request.getRequestDispatcher("/login");
-            //rd.forward(request, response);
-        }
+        String href = request.getParameter("href");
+        RequestDispatcher rd = request.getRequestDispatcher(href);
+        rd.forward(request, response);
     }
 }
