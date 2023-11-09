c2c30703649db1f3d0b237983b7939097d16beb8
Diego Camargo <diego.camargo@injunior.com.br>
2023-11-09 16:19:15 -0300

    Ajeitando conta.java

--- src/java/conta.java
+++ src/java/conta.java
@@ -1,20 +1,45 @@
+import models.UsuarioModel;
+import provider.UsuarioProvider;
+import provider.ContaProvider;
+
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Map;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.ContaModel;
 
-@WebServlet(urlPatterns = {"/banco"})
-public class banco extends HttpServlet {    
+@WebServlet(urlPatterns = {"/conta"})
+public class conta extends HttpServlet {    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        String nome = request.getParameter("nome");
-        try (PrintWriter out = response.getWriter()) {
-            //RequestDispatcher rd = request.getRequestDispatcher("/login");
-            //rd.forward(request, response);
+        String cpf = request.getParameter("cpf");
+        String tipo = request.getParameter("tipo");
+        if(cpf.isEmpty() || tipo.isEmpty()){
+            RequestDispatcher rd = request.getRequestDispatcher("/pages/cadConta/index.jsp");
+            rd.forward(request, response);
+        }else{
+            UsuarioModel usuario = UsuarioProvider.getUsuario(cpf);
+            if(usuario!=null){
+                Map<String, Object> dados = usuario.getDadosUsuario();
+                if("cliente".equals(dados.get("tipo"))){
+                    int id = ContaProvider.getTamanho();
+                    ContaModel conta = new ContaModel(id, (int) dados.get("id") ,tipo);
+                    ContaProvider.addConta(conta);
+                    RequestDispatcher rd = request.getRequestDispatcher("/pages/admin/index.jsp");
+                    rd.forward(request, response);
+                }else{
+                    RequestDispatcher rd = request.getRequestDispatcher("/pages/cadConta/index.jsp");
+                    rd.forward(request, response);
+                }
+            }else{
+                RequestDispatcher rd = request.getRequestDispatcher("/pages/cadConta/index.jsp");
+                rd.forward(request, response);
+            }  
         }
     }
 }
