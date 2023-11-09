fd927ec7109a01dfb90f1a832ee2c6e05c03c1a1
Diego Camargo <diego.camargo@injunior.com.br>
2023-11-09 16:20:20 -0300

    Criando usuario.java

---
+++ src/java/usuario.java
@@ -0,0 +1,34 @@
+import models.UsuarioModel;
+import provider.UsuarioProvider;
+
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/usuario"})
+public class usuario extends HttpServlet {
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String nome = request.getParameter("nome");
+        String tipo = request.getParameter("tipo");
+        String cpf = request.getParameter("cpf");
+        String email = request.getParameter("email");
+        if(cpf.isEmpty() || email.isEmpty() || nome.isEmpty()){
+            RequestDispatcher rd = request.getRequestDispatcher("/pages/cadUsuario/index.jsp");
+            rd.forward(request, response);
+        }else{
+            int id = UsuarioProvider.getTamanho();
+            UsuarioModel usuario = new UsuarioModel(id,nome,tipo, cpf,email);
+            UsuarioProvider.addUsuario(usuario);
+            RequestDispatcher rd = request.getRequestDispatcher("/pages/admin/index.jsp");
+            rd.forward(request, response);
+        }
+    }
+}
