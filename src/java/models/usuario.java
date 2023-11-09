43229f08969178415c8d90ce65949ffe88476eec
Diego Camargo <diego.camargo@injunior.com.br>
2023-11-09 16:15:30 -0300

    Ajeitando header

---
+++ src/java/models/usuario.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/banco"})
+public class banco extends HttpServlet {    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String nome = request.getParameter("nome");
+        try (PrintWriter out = response.getWriter()) {
+            //RequestDispatcher rd = request.getRequestDispatcher("/login");
+            //rd.forward(request, response);
+        }
+    }
+}
