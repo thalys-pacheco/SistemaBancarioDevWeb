d59d1cb0afb1a313723c7c13c7841f5dce1b129a
Diego Camargo <diego.camargo@injunior.com.br>
2023-11-09 16:19:54 -0300

    Ajeitando login.java

--- src/java/login.java
+++ src/java/login.java
@@ -1,4 +1,11 @@
+import models.UsuarioModel;
+import provider.UsuarioProvider;
+import models.ContaModel;
+import provider.ContaProvider;
+
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,20 +18,46 @@ public class login extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
+        
+        UsuarioModel adminDefault = new UsuarioModel(0,"Diego Admin","admin", "11111111111","diegoadmin@email.com");
+        UsuarioProvider.addUsuario(adminDefault);
+        
+        UsuarioModel clientDefault = new UsuarioModel(1,"Diego Cliente","cliente", "22222222222","diegocliente@email.com");
+        UsuarioProvider.addUsuario(clientDefault);
+        
+        ContaModel contaDefault = new ContaModel(0,1,"corrente");
+        ContaProvider.addConta(contaDefault);
+    
         String cpf = request.getParameter("cpf");
         String password = request.getParameter("password");
         if(cpf.isEmpty() || password.isEmpty()){
-            RequestDispatcher rd = request.getRequestDispatcher("/pages/login");
-            rd.forward(request, response);
-        }else if("11111111111".equals(cpf)){
-            RequestDispatcher rd = request.getRequestDispatcher("/pages/admin");
-            rd.forward(request, response);
-        }else if("22222222222".equals(cpf)){
-            RequestDispatcher rd = request.getRequestDispatcher("/pages/home");
+            RequestDispatcher rd = request.getRequestDispatcher("/pages/login/index.jsp");
             rd.forward(request, response);
         }else{
-            RequestDispatcher rd = request.getRequestDispatcher("/pages/login");
-            rd.forward(request, response);
+            UsuarioModel usuario = UsuarioProvider.getUsuario(cpf);
+            if(usuario!=null){
+                Map<String, Object> dados = usuario.getDadosUsuario();
+                if(password.equals(dados.get("senha"))){
+
+                    if("admin".equals(dados.get("tipo"))){
+                        RequestDispatcher rd = request.getRequestDispatcher("/pages/admin/index.jsp");
+                        rd.forward(request, response);
+                    }else{
+                        List<ContaModel> contas = ContaProvider.getContas((int) dados.get("id"));
+                        request.setAttribute("usuario", usuario);
+                        request.setAttribute("contas", contas);
+                        RequestDispatcher rd = request.getRequestDispatcher("/pages/home/index.jsp");
+                        rd.forward(request, response);
+                    }
+
+                }else{
+                    RequestDispatcher rd = request.getRequestDispatcher("/pages/login/index.jsp");
+                    rd.forward(request, response);
+                }
+            }else{
+                RequestDispatcher rd = request.getRequestDispatcher("/pages/login/index.jsp");
+                rd.forward(request, response);
+            }
         }
     }
 }
