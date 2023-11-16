909b29ceb174fd5230ee54fadc59de0a3e9f329c
Diego Camargo <diego.camargo@injunior.com.br>
2023-11-16 20:37:47 -0300

    Modificando o transacao.java

--- src/java/transacao.java
+++ src/java/transacao.java
@@ -1,45 +1,59 @@
-import models.UsuarioModel;
-import provider.UsuarioProvider;
+import provider.TransacaoProvider;
+import entidade.TransacaoEntidade;
 import provider.ContaProvider;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import models.ContaModel;
 
-@WebServlet(urlPatterns = {"/conta"})
-public class conta extends HttpServlet {    
+@WebServlet(urlPatterns = {"/transacao"})
+public class transacao extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        
+        int idConta = Integer.parseInt(request.getParameter("idConta"));
+        
+        List<TransacaoEntidade> transacoes = TransacaoProvider.getTransacoes( idConta);
+        request.setAttribute("transacoes", transacoes);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/pages/login/index.jsp");
+        rd.forward(request, response);
+        
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        String cpf = request.getParameter("cpf");
         String tipo = request.getParameter("tipo");
-        if(cpf.isEmpty() || tipo.isEmpty()){
-            RequestDispatcher rd = request.getRequestDispatcher("/pages/cadConta/index.jsp");
-            rd.forward(request, response);
-        }else{
-            UsuarioModel usuario = UsuarioProvider.getUsuario(cpf);
-            if(usuario!=null){
-                Map<String, Object> dados = usuario.getDadosUsuario();
-                if("cliente".equals(dados.get("tipo"))){
-                    int id = ContaProvider.getTamanho();
-                    ContaModel conta = new ContaModel(id, (int) dados.get("id") ,tipo);
-                    ContaProvider.addConta(conta);
-                    RequestDispatcher rd = request.getRequestDispatcher("/pages/admin/index.jsp");
-                    rd.forward(request, response);
-                }else{
-                    RequestDispatcher rd = request.getRequestDispatcher("/pages/cadConta/index.jsp");
-                    rd.forward(request, response);
-                }
-            }else{
-                RequestDispatcher rd = request.getRequestDispatcher("/pages/cadConta/index.jsp");
-                rd.forward(request, response);
-            }  
+        int idConta = Integer.parseInt(request.getParameter("idConta"));
+        double valor = Double.parseDouble(request.getParameter("valor"));
+        
+        switch(tipo){
+            case "Deposito":
+                TransacaoProvider.deposito( idConta, valor);
+                ContaProvider.deposito(idConta,valor);
+                break;
+            case "Saque":
+                TransacaoProvider.saque(idConta, valor);
+                ContaProvider.saque(idConta,valor);
+                break;
+            case "Transferencia":
+                int idDestinatario = Integer.parseInt(request.getParameter("idDestinatario"));
+                TransacaoProvider.transferencia( idConta, idDestinatario,valor);
+                ContaProvider.saque(idConta,valor);
+                ContaProvider.deposito(idDestinatario,valor);
+                break;
         }
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/pages/home/index.jsp");
+        rd.forward(request, response);
+        
     }
 }
